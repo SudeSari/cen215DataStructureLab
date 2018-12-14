0ebb28c11e0599b9142170f06d8e2650a593b8e3
Abbas Elmas <abbaselmas@gmail.com>
2018-12-14 09:18:37 +0300

    some more students send queue implementations

--- 8Queue/UgurInalwithArray/Source.c
+++ 8Queue/UgurInalwithArray/Source.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAXSIZE 100  // Max size of Queue
 
--- 8Queue/UgurInalwithLL/Source.c
+++ 8Queue/UgurInalwithLL/Source.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct list {
 	int data;
